c6a441736bbcd7a37a0d695e2e5fec1805afa234
Dragomir Tsanev <texh00@gmail.com>
2015-09-15 17:03:23 +0300

    Homework

---
+++ 2.Introduction_to_Programming-Homework/15.After_Ten_Years/AfterTenYears.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class AfterTenYears
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your age:");
+        int Age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Аfter 10 years you will be: {0}", Age + 10 + " years.");
+    }
+}
