54c81fd026f27f7f617cfbeadf215bc3e96ca8ac
Dragomir Tsanev <texh00@gmail.com>
2015-10-13 09:50:55 +0300

    homework

---
+++ 5.OperatorsExpressionsAndStatement/5.ThirdDigitIs7/ThirdDigitIs7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5.ThirdDigitIs7
+{
+    class ThirdDigitIs7
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter an integer number:");
+            int number = int.Parse(Console.ReadLine());
+            int pathToDigit = number / 100;
+            int thirdDigit = pathToDigit % 10;
+            bool isSeven = thirdDigit == 7;
+            Console.WriteLine(isSeven);
+        }
+    }
+}
---
+++ 5.OperatorsExpressionsAndStatement/7.PointInACircle/PointInACircle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _7.PointInACircle
+{
+    internal class PointInACircle
+    {
+        private static void Main()
+        {
+            Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("y=");
+            double y = double.Parse(Console.ReadLine());
+            int radiusCircle = 2;
+
+            double pointDistance = Math.Sqrt((x * x) + (y * y));
+
+            bool checkPoint = (pointDistance <= radiusCircle);
+            if (checkPoint)
+            {
+                Console.WriteLine("inside: {0}", checkPoint);
+            }
+            else
+            {
+                Console.WriteLine("inside: {0}", checkPoint);
+            }
+        }
+    }
+}
