0b774eac177533ce3bf5acdbba4b03566abca20a
Dragomir Tsanev <texh00@gmail.com>
2015-10-15 17:44:51 +0300

    Homework

---
+++ 6.ConditionalStatementsHomework/8.DigitАsWord/DigitАsWord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _8.DigitАsWord
+{
+    internal class DigitАsWord
+    {
+        private static void Main()
+        {
+            Console.Write("Write a digit in range [0-9] :");
+            string digit = Console.ReadLine();
+            switch (digit)
+            {
+                case "0":
+                    Console.WriteLine("zero");
+                    break;
+                case "1":
+                    Console.WriteLine("one");
+                    break;
+                case "2":
+                    Console.WriteLine("two");
+                    break;
+                case "3":
+                    Console.WriteLine("three");
+                    break;
+                case "4":
+                    Console.WriteLine("four");
+                    break;
+                case "5":
+                    Console.WriteLine("five");
+                    break;
+                case "6":
+                    Console.WriteLine("six");
+                    break;
+                case "7":
+                    Console.WriteLine("seven");
+                    break;
+                case "8":
+                    Console.WriteLine("eight");
+                    break;
+                case "9":
+                    Console.WriteLine("nine");
+                    break;
+                default:
+                    Console.WriteLine("not a digit");
+                    break;
+            }
+        }
+    }
+}
