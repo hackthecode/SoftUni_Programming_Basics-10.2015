ef40f45158340948fb6cb14a0d24ff884644d50b
Dragomir Tsanev <texh00@gmail.com>
2015-11-12 23:03:13 +0200

    update

---
+++ 4.ConsoleInputOutput/07.SumOfFiveNumbers/SumOfFiveNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07.SumOfFiveNumbers
+{
+    class SumOfFiveNumbers
+    {
+        static void Main()
+        {
+            Console.Write("Please enter 5 numbers divided by space:");
+            string[] input = Console.ReadLine().Split(' ');
+            double sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double n = double.Parse(input[i]);
+                sum += n;
+            }
+            Console.WriteLine("sum: {0}",sum);
+        }
+    }
+}
---
+++ 4.ConsoleInputOutput/SumOf3Numbers/SumOf3Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOf3Numbers
+{
+    class SumOf3Numbers
+    {
+        static void Main()
+        {
+            // input - 3 real numbers from user 
+            Console.Write("Enter the first real number: ");
+            double firtNumber = double.Parse(Console.ReadLine());
+            Console.Write("Enter the second real number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+            Console.Write("Enter the third real number: ");
+            double thirdNumber = double.Parse(Console.ReadLine());
+            // calculate sum of 3 real numbers
+            double sum = firtNumber + secondNumber + thirdNumber;
+            Console.WriteLine("sum = " + sum );
+        }
+    }
+}
